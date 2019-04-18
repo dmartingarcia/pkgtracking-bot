25023d74b4b26e22cba33d76f3837471dbd80fc3
David Martin Garcia <davidmartingarcia0@gmail.com>
2019-04-18 13:51:54 +0200

    modify schema to make ended field not nil and false by default

--- priv/repo/migrations/20181119003933_create_tracking_codes.exs
+++ priv/repo/migrations/20181119003933_create_tracking_codes.exs
@@ -5,7 +5,7 @@ defmodule App.Repo.Migrations.CreateTrackingCodes do
     create table(:tracking_codes) do
       add :code, :string, null: false
       add :chat_id, :integer, null: false
-      add :ended, :boolean
+      add :ended, :boolean, null: false, default: false
 
       timestamps()
     end
